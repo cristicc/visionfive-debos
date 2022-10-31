d28508662b9576cebff5df136e5a73f5f22180ed
Cristian Ciocaltea <cristian.ciocaltea@gmail.com>
2022-10-31 22:37:09 +0200

    debos: Improve reporting in debos.sh
    Use an rsync alias to get rid of unnecessary file sync status information.
    Additionally, print the path of the created disk image file.
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@gmail.com>

--- debos/debos.sh
+++ debos/debos.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SCRIPT_DIR=$(readlink -mn "$0")
+SCRIPT_DIR=$(realpath -z "$0")
 SCRIPT_DIR=${SCRIPT_DIR%/*}
 
 INSTALL_DIR=${SCRIPT_DIR}/../work/debos
@@ -23,16 +23,27 @@ fi
 DEV_OPTS="${DEV_OPTS} -t create_image:yes"
 #DEBUG_OPTS="--debug-shell --verbose"
 
+echo "Syncing build resources"
+alias rsync='rsync -av --omit-dir-times --info=flist0,stats0,remove --out-format=" %i %n"'
+
 mkdir -p ${INSTALL_DIR} && cd ${INSTALL_DIR}
-rsync -av --exclude="/${0##*/}" ${SCRIPT_DIR}/ ${INSTALL_DIR}/
+rsync --exclude="/${0##*/}" ${SCRIPT_DIR}/ ${INSTALL_DIR}/
 
 BIN_DIR=${INSTALL_DIR}/scripts/bin
 mkdir -p ${BIN_DIR}
-rsync -av ${INSTALL_DIR}/../linux/linux-image-*_riscv64.deb ${BIN_DIR}/
-rsync -av ${INSTALL_DIR}/../grub/dist/grubriscv64.efi ${BIN_DIR}/
+rsync ${INSTALL_DIR}/../linux/linux-image-*_riscv64.deb ${BIN_DIR}/
+rsync ${INSTALL_DIR}/../grub/dist/grubriscv64.efi ${BIN_DIR}/
+
+IMG_FILE=${INSTALL_DIR}/debian-riscv64.img.gz
+rm -rf "${IMG_FILE}"
 
 docker run --device /dev/kvm -u $(id -u) -w /recipes \
     --mount "type=bind,source=$(pwd),destination=/recipes" \
     --security-opt label=disable --rm -it \
     ${DEBOS_IMAGE} ${DEV_OPTS} ${DEBUG_OPTS} "$@" \
     debimage-riscv64.yaml
+
+[ -f "$IMG_FILE" ] && {
+    IMG_FILE=$(realpath "${IMG_FILE}")
+    printf "Created disk img: %s\n" "${IMG_FILE}"
+}

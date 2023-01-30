cristicc/visionfive-debos

commit 28908470d0f7aa9e898d5f037e1dd3c7e1e83bd6

tools: Refactor tftp-boot.py
Generalize send_uboot_cmd() to allow waiting for a command specific
message string.

Additionally, define constants for the board specific data used in
tftp_boot_linux() and properly wait for booting the Linux kernel and the
shell prompt to show up.

Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@gmail.com>

tools/tftp-boot.py
@@ -13,6 +13,34 @@ import pexpect.fdpexpect
 import serial
 
 UBOOT_PROMPT = "VisionFive #"
+UBOOT_ERROR_MSGS = [
+    "Resetting CPU"
+    "Must RESET board to recover"
+    "TIMEOUT"
+    "Retry count exceeded"
+    "Retry time exceeded; starting again"
+    "ERROR: The remote end did not respond in time."
+    "File not found"
+    "Bad Linux ARM64 Image magic!"
+    "Wrong Ramdisk Image Format"
+    "Ramdisk image is corrupt or invalid"
+    "ERROR: Failed to allocate"
+    "TFTP error: trying to overwrite reserved memory"
+    "Bad Linux RISCV Image magic!"
+    "Wrong Image Format for boot"
+    "ERROR: Did not find a cmdline Flattened Device Tree"
+    "ERROR: RD image overlaps OS image"
+]
+
+UBOOT_KERNEL_ADDR = "0x84000000"
+UBOOT_DTB_ADDR = "0x88000000"
+UBOOT_RAMDISK_ADDR = "0x88300000"
+
+TFTP_SERVER_IP = "192.168.1.90"
+LINUX_KERNEL_IMG_DIR = "linux/build/arch/riscv/boot"
+
+LINUX_KERNEL_START_MSG = "Linux version [0-9]"
+SHELL_PROMPT = "/ # "
 
 
 def wait_uboot_prompt(con):
@@ -36,31 +64,20 @@ def wait_uboot_prompt(con):
     raise Exception("Failed to get U-Boot prompt")
 
 
-def send_cmd(con, cmd):
+def wait_shell_prompt(con):
+    print()
+    print("> Waiting for Shell prompt..")
+    con.expect(SHELL_PROMPT, timeout=60)
+
+
+def send_uboot_cmd(con, cmd, cmd_expect=UBOOT_PROMPT):
+    msgs = UBOOT_ERROR_MSGS.copy()
+    msgs.insert(0, cmd_expect)
+
     try:
         con.sendline(cmd)
 
-        res = con.expect(
-            [
-                UBOOT_PROMPT,
-                "Resetting CPU",
-                "Must RESET board to recover",
-                "TIMEOUT",
-                "Retry count exceeded",
-                "Retry time exceeded; starting again",
-                "File not found",
-                "Wrong Ramdisk Image Format",
-                "Ramdisk image is corrupt or invalid",
-                "TFTP error: trying to overwrite reserved memory",
-                "Bad Linux RISCV Image magic!",
-                "Wrong Image Format for boot",
-                "ERROR: Failed to allocate",
-                "ERROR: The remote end did not respond in time.",
-                "ERROR: Did not find a cmdline Flattened Device Tree",
-                "ERROR: RD image overlaps OS image",
-            ]
-        )
-
+        res = con.expect(msgs)
         if res > 0:
             raise Exception("Unexpected response: %s" % con.match.group(0))
 
@@ -70,26 +87,34 @@ def send_cmd(con, cmd):
 
 def tftp_boot_linux(con):
     try:
-        send_cmd(con, "setenv autoload no")
-        send_cmd(con, "setenv initrd_high 0xffffffffffffffff")
-        send_cmd(con, "setenv fdt_high 0xffffffffffffffff")
-        send_cmd(con, "dhcp")
-        send_cmd(con, "setenv serverip 192.168.1.90")
-        send_cmd(
-            con, "tftpboot 0x84000000 linux/build/arch/riscv/boot/Image"
-        )
-        send_cmd(
+        send_uboot_cmd(con, "setenv autoload no")
+        send_uboot_cmd(con, "setenv initrd_high 0xffffffffffffffff")
+        send_uboot_cmd(con, "setenv fdt_high 0xffffffffffffffff")
+        send_uboot_cmd(con, "dhcp")
+        send_uboot_cmd(con, f"setenv serverip {TFTP_SERVER_IP}")
+        send_uboot_cmd(
             con,
-            "tftpboot 0x88000000 linux/build/arch/riscv/boot/dts/starfive/jh7100-starfive-visionfive-v1.dtb",
+            f"tftpboot {UBOOT_KERNEL_ADDR} {LINUX_KERNEL_IMG_DIR}/Image",
         )
-        send_cmd(con, "tftpboot 0x88300000 ramdisk/rootfs.cpio.gz.uboot")
-        send_cmd(
+        send_uboot_cmd(
+            con,
+            f"tftpboot {UBOOT_DTB_ADDR} {LINUX_KERNEL_IMG_DIR}"
+            + "/dts/starfive/jh7100-starfive-visionfive-v1.dtb",
+        )
+        send_uboot_cmd(
+            con, f"tftpboot {UBOOT_RAMDISK_ADDR} ramdisk/rootfs.cpio.gz.uboot"
+        )
+        send_uboot_cmd(
             con,
             "setenv bootargs"
             + " 'console=ttyS0,115200n8 root=/dev/ram0"
             + " console_msg_format=syslog earlycon ip=dhcp ip=dhcp'",
         )
-        send_cmd(con, "booti 0x84000000 0x88300000 0x88000000")
+        send_uboot_cmd(
+            con,
+            f"booti {UBOOT_KERNEL_ADDR} {UBOOT_RAMDISK_ADDR} {UBOOT_DTB_ADDR}",
+            LINUX_KERNEL_START_MSG
+        )
 
     except Exception as e:
         print()
@@ -125,7 +150,7 @@ with pexpect.fdpexpect.fdspawn(
 ) as con:
     wait_uboot_prompt(con)
     tftp_boot_linux(con)
-    ser.close()
+    wait_shell_prompt(con)
 
 print
-print("== Done! ==")
+print("> Done")

commit bf7952277a43d1e0c76be2dc652a89d03cdf849b

tools/tftp-boot: Fix handling of U-Boot error messages
This is a regression introduced during a previous refactoring.

Fixes: 28908470d0f7 ("tools: Refactor tftp-boot.py")
Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@gmail.com>

tools/tftp-boot.py
@@ -12,23 +12,25 @@ import pexpect.fdpexpect
 import serial
 
 UBOOT_PROMPT = "VisionFive #"
+
 UBOOT_ERROR_MSGS = [
-    "Resetting CPU"
-    "Must RESET board to recover"
-    "TIMEOUT"
-    "Retry count exceeded"
-    "Retry time exceeded; starting again"
-    "ERROR: The remote end did not respond in time."
-    "File not found"
-    "Bad Linux ARM64 Image magic!"
-    "Wrong Ramdisk Image Format"
-    "Ramdisk image is corrupt or invalid"
-    "ERROR: Failed to allocate"
-    "TFTP error: trying to overwrite reserved memory"
-    "Bad Linux RISCV Image magic!"
-    "Wrong Image Format for boot"
-    "ERROR: Did not find a cmdline Flattened Device Tree"
-    "ERROR: RD image overlaps OS image"
+    "Resetting CPU",
+    "Must RESET board to recover",
+    "TIMEOUT",
+    "Retry count exceeded",
+    "Retry time exceeded; starting again",
+    "ERROR: The remote end did not respond in time.",
+    "File not found",
+    "Bad Linux ARM64 Image magic!",
+    "Wrong Ramdisk Image Format",
+    "Ramdisk image is corrupt or invalid",
+    "ERROR: Failed to allocate",
+    "TFTP error: \\w+",
+    "TFTP server died",
+    "Bad Linux RISCV Image magic!",
+    "Wrong Image Format for boot",
+    "ERROR: Did not find a cmdline Flattened Device Tree",
+    "ERROR: RD image overlaps OS image",
 ]
 
 UBOOT_KERNEL_ADDR = "0x84000000"